9040665bf6dae09a7dd33c4f970b699ea310b73f
Görkem <emircan9921@outlook.com>
2026-01-13 13:06:32 +0300

    Updated bottom navigation bar.

--- lib/view/widget/home/home_bottom_nav_bar.dart
+++ lib/view/widget/home/home_bottom_nav_bar.dart
@@ -16,6 +16,7 @@ class HomeBottomNavBar extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(32.0),
       child: Container(
+        height: 70,
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(40),
@@ -30,22 +31,28 @@ class HomeBottomNavBar extends StatelessWidget {
         ),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(40),
-          child: BottomNavigationBar(
-            currentIndex: currentIndex,
-            onTap: onTap,
-            backgroundColor: Colors.white,
-            selectedItemColor: ApplicationColors.accent,
-            unselectedItemColor: Colors.grey.shade400,
-            showSelectedLabels: true,
-            showUnselectedLabels: true,
-            type: BottomNavigationBarType.fixed,
-            elevation: 0,
-            items: [
-              _buildNavItem(Icons.home_rounded, Icons.home_outlined, "Anasayfa", 0),
-              _buildNavItem(Icons.flash_on_rounded, Icons.gamepad_outlined, "Alıştırma", 1),
-              _buildNavItem(Icons.person_rounded, Icons.person_outline, "Profil", 2),
-            ],
-          ),
+          child: MediaQuery.removePadding(
+            context: context,
+            removeBottom: true,
+            child: BottomNavigationBar(
+              currentIndex: currentIndex,
+              onTap: onTap,
+              backgroundColor: Colors.white,
+              selectedItemColor: ApplicationColors.accent,
+              unselectedItemColor: Colors.grey.shade400,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              selectedFontSize: 0,
+              unselectedFontSize: 0,
+              type: BottomNavigationBarType.fixed,
+              elevation: 0,
+              items: [
+                _buildNavItem(Icons.home_rounded, Icons.home_outlined, "Anasayfa", 0),
+                _buildNavItem(Icons.insert_drive_file_rounded, Icons.insert_drive_file_outlined, "Rapor", 1),
+                _buildNavItem(Icons.person_rounded, Icons.person_outline, "Profil", 2),
+              ],
+            ),
+          )
         ),
       ),
     );
@@ -56,7 +63,7 @@ class HomeBottomNavBar extends StatelessWidget {
     return BottomNavigationBarItem(
       icon: AnimatedContainer(
         duration: const Duration(milliseconds: 200),
-        padding: const EdgeInsets.all(12),
+        padding: const EdgeInsets.all(10),
         decoration: isSelected
             ? BoxDecoration(
           color: ApplicationColors.accent.withValues(alpha: 0.1),
@@ -68,7 +75,7 @@ class HomeBottomNavBar extends StatelessWidget {
         ),
         child: Icon(
           isSelected ? activeIcon : inactiveIcon,
-          size: 26,
+          size: 28,
         ),
       ),
       label: label,
